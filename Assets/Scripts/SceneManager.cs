1fa1ac1e782e0c133b073c1163cc3dac90e111b3
Woofplayer <salma.makinen@gmail.com>
2021-05-04 19:28:50 +0300

    kenttadesign

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
